613432cb361c834d92a1af4c5007330d206733c9
bigwudan <bigwudan@qq.com>
2018-02-03 22:19:57 +0800

    11

--- test.c
+++ test.c
@@ -1,14 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 
 int main()
 {
-    int array[2][3] = {{1,2,3}, {4,5,6}};
-    // printf("*array=%d\n", *array);
-    printf("array=%x\n", array);
-    printf("*array=%x\n", *array);
+    time_t timep;
+    struct tm *p;
+    time(&timep);
+    p = gmtime(&timep);
 
+    printf("%d-%d-%d\n", (1900+p->tm_year), (1+p->tm_mon), p->tm_mday);
+
+    printf("%d-%d\n", 24 - p->tm_hour, p->tm_min);
+
+    char filename[20] = {0};
+
+    sprintf(filename, "%d-%d-%d-%d-%d",(1900+p->tm_year), (1+p->tm_mon), p->tm_mday,  24 - p->tm_hour, p->tm_min);
+
+    printf("file=%s\n", filename);
 
     return 0;   
 }
